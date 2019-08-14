c9bf556c98fc73adf965a934a8427e2fd2a57b44
jer212 <31511450+jer212@users.noreply.github.com>
2019-08-13 20:52:35 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ function respond() {
       botRegexSiege = /^\/siege/; botRegexOW = /^\/ratings/; 
       botRegexFaggot = /^\/faggot/; botRegexboot = /^\/boot/;
       botRegexProp = /^\/prop/;botRegexKys = /^\/kys/; botRegexSlam = /^\/slam/; botRegexskip = /^\/skip/;
-      botRegexGian = /^\/gian/; botRegexrip = /^\/rip/; botRegexShaki = /^\/shaki/;
+      botRegexGian = /^\/gian/; botRegexrip = /^\/rip/; botRegexniños = /^\/ninos/;
       botRegexDaf = /^\/dafuq/; botRegexMA32 = /^\/pending/; botRegexTrade = /^\/trade/; 
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
@@ -63,9 +63,9 @@ function respond() {
     postMessage("http://gph.is/1CJWpHS");
     this.res.end();
   } 
-  else if(request.text && botRegexShaki.test(request.text)) {
+  else if(request.text && botRegexniños.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://i.groupme.com/305x375.jpeg.a8dd3aa18c7a4963a553e6dc16bcb100.large");
+    postMessage("https://i.postimg.cc/j5zGD1BN/3-kids-with-school-bags-and-books-2.jpg");
     this.res.end();
   } 
   else if(request.text && botRegexDL.test(request.text)) {
